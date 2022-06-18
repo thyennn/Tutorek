9c63537dd2fe9c83010197bb4679369697b65c50
Mateusz Godlewski <54773126+thyennn@users.noreply.github.com>
2022-06-18 15:50:34 +0000

    Custom Help Command added

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from discord.ext import commands
 import discord
 from scoreboard_pilna import ScoreBoard
 import os
+from keep_alive import keep_alive
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='/', help_command=None)
 DISCORD_BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -51,39 +52,42 @@ async def pilna_d(ctx, date):
 
 @bot.command()
 async def pilna_m(ctx, month):
-    month = int(month)
-    result = ScoreBoard().get_month_stats(month)
+    try:
+        month = int(month)
+        result = ScoreBoard().get_month_stats(month)
 
-    if month == 1:
-        name = "January"
-    elif month == 2:
-        name = "February"
-    elif month == 3:
-        name = "March"
-    elif month == 4:
-        name = "April"
-    elif month == 5:
-        name = "May"
-    elif month == 6:
-        name = "June"
-    elif month == 7:
-        name = "July"
-    elif month == 8:
-        name = "August"
-    elif month == 9:
-        name = "September"
-    elif month == 10:
-        name = "October"
-    elif month == 11:
-        name = "November"
-    elif month == 12:
-        name = "December"
+        if month == 1:
+            name = "January"
+        elif month == 2:
+            name = "February"
+        elif month == 3:
+            name = "March"
+        elif month == 4:
+            name = "April"
+        elif month == 5:
+            name = "May"
+        elif month == 6:
+            name = "June"
+        elif month == 7:
+            name = "July"
+        elif month == 8:
+            name = "August"
+        elif month == 9:
+            name = "September"
+        elif month == 10:
+            name = "October"
+        elif month == 11:
+            name = "November"
+        elif month == 12:
+            name = "December"
 
-    if type(result) == str:
-        await ctx.channel.send(result)
-    else:
-        result = get_embed(result, name)
-        await ctx.channel.send(embed=result)
+        if type(result) == str:
+            await ctx.channel.send(result)
+        else:
+            result = get_embed(result, name)
+            await ctx.channel.send(embed=result)
+    except:
+        await ctx.channel.send("Error: Not Found :hear_no_evil:")
 
 
 @bot.command()
@@ -101,13 +105,29 @@ async def pilna(ctx):
 
 
 @bot.command()
-async def t_help(ctx):
-    result = """
-        Available Commands:\t
-        /t_help\t
-        /pilna \t\t[t -> today / date e.g. '2022-06-17']
-    """
-    await ctx.channel.send(result)
+async def help(ctx):
+    result = discord.Embed(title="Help")
+    result.add_field(name="Commands",
+                     value="/help\n" +
+                           "/pilna\n" +
+                           "/pilna_t\n" +
+                           "/pilna_d\n" +
+                           "/pilna_m")
+
+    result.add_field(name=":beers:",
+                     value="Shows this message\n" +
+                     "Displays all statistics from the sheet\n" +
+                     "... for today\n" +
+                     "... from the entered date as a parameter (e.g. 2022-06-16)\n" +
+                     "... for the entered month as parameter (1-12)")
+    
+    await ctx.channel.send(embed=result)
 
 
-bot.run(DISCORD_BOT_TOKEN)
+keep_alive()
+try:
+    bot.run(DISCORD_BOT_TOKEN)
+except discord.errors.HTTPException:
+    print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
+    os.system("python restarter.py")
+    os.system('kill 1')
